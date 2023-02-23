7cdbcd0438e93121e884e5ac96f3b32e7e6e00db
Gustavo <gustavoamorilsilva@gmail.com>
2023-02-23 13:48:29 -0300

    teste push

--- api/Program.cs
+++ api/Program.cs
@@ -7,7 +7,7 @@ var app = builder.Build();
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
 
-//adiconar produtoss
+//adiconar produto
 app.MapPost("/products", (Product product) => {
     ProductRepository.Add(product);
     return Results.Created($"/products/{product.Code}", product.Code);
